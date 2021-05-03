cb0c4c01e5a2d638862f87c31fba4afb716b9aeb
BaldinhO17 <baldinho.barros17@gmail.com>
2021-05-03 14:46:43 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from "./components/NavBar";
+import DataTable from "./components/DataTable";
+import Footer from "./components/Footer";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <NavBar />
+      <main className="container">
         <h1 className="text-primary">Hello, World</h1>
-      </header>
-    </div>
+        <DataTable />
+      </main>
+      <Footer />
+    </>
   );
 }
 
